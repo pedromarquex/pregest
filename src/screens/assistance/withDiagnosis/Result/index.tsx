3f339b812a97abaf025433dd52de08e2b0f1c30c
Pedro Marques <pedro.marques@ufpi.edu.br>
2023-12-06 01:40:33 -0300

    Refactor code to handle different risk levels in result screen

--- src/screens/assistance/withDiagnosis/Result/index.tsx
+++ src/screens/assistance/withDiagnosis/Result/index.tsx
@@ -8,43 +8,108 @@ import { Dialog } from '../../../../components/Dialog/dialog'
 import { Title } from '../../../../components/Title'
 import { type AssistanceStackScreenProps } from '../../../../routes/assistance/assistanceStack.types'
 
+enum Risk {
+  WITH_RISK = 'WITH_RISK',
+  WITHOUT_RISK = 'WITHOUT_RISK'
+}
+
+enum RiskMapper {
+  WITH_RISK = 'COM SINAIS DE GRAVIDADE',
+  WITHOUT_RISK = 'SEM SINAIS DE GRAVIDADE'
+}
+
 export function WithResult ({ navigation, route }: AssistanceStackScreenProps<'Result'>): JSX.Element {
   const { data } = route.params
   // log data pretified
-  console.log(JSON.stringify(data, null, 2))
+  // console.log(JSON.stringify(data, null, 2))
 
   const [open, setOpen] = React.useState(true)
+  const [risk, setRisk] = React.useState<Risk>(Risk.WITH_RISK)
 
-  const contentText = [
-    'Prescrever suplementação de cálcio (carbonato de cálcio)- 1,0 a 2,0g/dia, por via oral, preferencialmente antes do almoço e jantar, para mulheres com dieta pobre em cálcio.'
-  ]
+  const modalText = {
+    WITHOUT_RISK: 'Manter consulta pré-natal semanalmente.',
+    WITH_RISK: 'Encaminhar imediatamente para  à unidade de referência hospitalar.'
+  }
 
-  const titles = [
-    'Intervenções'
-  ]
+  const title = 'Descrição das intervenções'
+
+  const contentText = {
+    WITHOUT_RISK: {
+      HEADLINE: 'Escuta ativa da mulher e seus acompanhantes, esclarecendo dúvidas e informando o será realizado durante a consulta  e as condutas a serem adotas;',
+      MOTHER: [
+        'Controle rígido da pressão arterial (4/4h) e dos sintomas.',
+        'Prescrever e administrar anti-hipertensivos se a pressão arterial atingir níveis ≥ 150 ou 100 mmHg, níveis ≥ 140 ou 90 mmHg persistentes ou ainda se a paciente se mostrar sintomática'
+      ],
+      FETUS: [
+        'Verificar altura do fundo uterina',
+        'Avaliar movimentação fetal',
+        'Verificar batimentos cardiofetais',
+        'Solicitar exames para vigilância do bem-estar e do crescimento fetal.'
+      ]
+    },
+    WITH_RISK: {
+      HEADLINE: 'Esclarecimento do grau de risco para paciente e responsável;\nSolicitar transporte;',
+      MOTHER: [
+        'Escuta ativa da mulher e seus acompanhantes, esclarecendo dúvidas e informando o será realizado durante a consulta  e as condutas a serem adotas;',
+        'Anamnese e exame clínico-obstétrico;',
+        'Verificar Pressão arterial e orientar a paciente a aferir a pressão arterial diariamente;',
+        'Manter controle de peso;',
+        'Monitorar semanalmente exames laboratoriais;',
+        'Orientar e acompanhar quanto à dieta monossódica e hiperproteica;',
+        'Orientar períodos de repouso diários em decúbito lateral;',
+        'Orientar sobre a importância de entrar em contato (via telefone, telemedicina) ou de procurar hospital em caso de alteração em  quadro clínico;',
+        'Orientar e monitorar sinais e sintomas de iminência de eclâmpsia;',
+        'Não prescrever diuréticos;',
+        'Registro em prontuário e cartão da gestante, inclusive registro de intercorrências/urgências que requeiram avaliação hospitalar em situações que não necessitem de internação;',
+        'Encaminhar a gestante para unidade de referência hospitalar em casos de pré-eclâmpsia com sinais gravidade.'
+      ],
+      FETUS: [
+        'Verificar altura do fundo uterina',
+        'Avaliar movimentação fetal',
+        'Verificar batimentos cardiofetais',
+        'Solicitar exames para vigilância do bem-estar e do crescimento fetal.'
+      ]
+    }
+  }
 
   const finalText = 'Prescrever aspirina em baixa dose (50-150 mg/dia), por via oral, a noite, antes de dormir- Iniciar entre 12 e 16 semanas de gestação e suspender após 36ª semana de gestação;'
 
   return (
     <Background style={styles.container}>
       <BodyContainer>
-        <Title text="ALTO RISCO" style={{ textAlign: 'center' }} />
-        {contentText.map((text, index) => (
-          <View key={index}>
-            <Title text={titles[index]} />
-            <BodyText text={text} withDivider />
-          </View>
-        ))}
-        <BodyText text={finalText} />
+        <Title
+          text={`PRÉ-ECLÂMPSIA ${RiskMapper[risk]}`}
+          style={{ textAlign: 'center' }}
+        />
+        <Title text={title} />
+        <BodyText text={contentText[risk].HEADLINE} withDivider />
+        <Title
+          text={risk === Risk.WITH_RISK ? 'Avaliação do bem-estar materno' : 'Estabilização clínica materna até chegada de transporte'}
+        />
+        <BodyText
+          text={
+            '- ' + contentText[risk].MOTHER.join('\n- ')
+          }
+        />
+        <Title text="Avaliação do bem-estar fetal" />
+        <BodyText
+          text={
+            '- ' + contentText[risk].FETUS.join('\n- ')
+          }
+        />
       </BodyContainer>
+
       <Dialog
         open={open}
         setOpen={setOpen}
       >
         <View>
-          <Title text="ALTO RISCO" style={{ textAlign: 'center' }} />
+          <Title
+            text={`PRÉ-ECLÂMPSIA ${RiskMapper[risk]}`}
+            style={{ textAlign: 'center' }}
+          />
           <BodyText
-            text={`PACIENTE DE ALTO RISCO${'\n'}Fique atento às intervenções recomendadas`}
+            text={modalText[risk]}
             style={{ textAlign: 'center' }}
           />
         </View>
--- src/screens/assistance/withoutDiagnosis/Result/index.tsx
+++ src/screens/assistance/withoutDiagnosis/Result/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Background } from '../../../../components/Background'
 import { BodyContainer } from '../../../../components/BodyContainer'
@@ -8,39 +8,62 @@ import { Dialog } from '../../../../components/Dialog/dialog'
 import { Title } from '../../../../components/Title'
 import { type AssistanceStackScreenProps } from '../../../../routes/assistance/assistanceStack.types'
 
+enum Risk {
+  HIGH_RISK = 'HIGH_RISK',
+  MODERATE_RISK = 'MODERATE_RISK',
+  LOW_RISK = 'LOW_RISK'
+}
+
+enum RiskMapper {
+  HIGH_RISK = 'ALTO RISCO',
+  MODERATE_RISK = 'RISCO MODERADO',
+  LOW_RISK = 'BAIXO RISCO'
+}
+
 export function Result ({ navigation, route }: AssistanceStackScreenProps<'Result'>): JSX.Element {
   const [open, setOpen] = React.useState(true)
 
-  const contentText = [
-    'Prescrever suplementação de cálcio (carbonato de cálcio)- 1,0 a 2,0g/dia, por via oral, preferencialmente antes do almoço e jantar, para mulheres com dieta pobre em cálcio.'
-  ]
+  const [risk, setRisk] = useState<Risk>(Risk.HIGH_RISK)
 
-  const titles = [
-    'Intervenções'
-  ]
+  const contentText = {
+    HIGH_RISK: 'Prescrever suplementação de cálcio (carbonato de cálcio)- 1,0 a 2,0g/dia, por via oral, preferencialmente antes do almoço e jantar, para mulheres com dieta pobre em cálcio.',
+    MODERATE_RISK: 'Prescrever suplementação de cálcio  (carbonato de cálcio)- 1,0 a 2,0g/dia, por via oral, preferencialmente antes do almoço e jantar, para mulheres com dieta pobre em cálcio.',
+    LOW_RISK: 'Prescrever suplementação de cálcio de 1,0 a 2,0g /dia, por via oral, preferencialmente antes do almoço e jantar, para mulheres com dieta pobre em cálcio.'
+  }
 
-  const finalText = 'Prescrever aspirina em baixa dose (50-150 mg/dia), por via oral, a noite, antes de dormir- Iniciar entre 12 e 16 semanas de gestação e suspender após 36ª semana de gestação;'
+  const title = 'Intervenções'
+
+  const finalText = {
+    HIGH_RISK: 'Prescrever aspirina em baixa dose (50-150 mg/dia), por via oral, a noite, antes de dormir- Iniciar entre 12 e 16 semanas de gestação e suspender após 36ª semana de gestação;',
+    MODERATE_RISK: 'Prescrever aspirina em baixa dose (50-150 mg/dia), por via oral, a noite, antes de dormir- Iniciar entre 12 e 16 semanas de gestação e suspender após 36ª semana de gestação;',
+    LOW_RISK: 'Não há necessidade em prescrever Aspirina.'
+  }
 
   return (
     <Background style={styles.container} contentContainerStyle={{ paddingBottom: 150 }}>
       <BodyContainer>
-        <Title text="ALTO RISCO" style={{ textAlign: 'center' }} />
-        {contentText.map((text, index) => (
-          <View key={index}>
-            <Title text={titles[index]} />
-            <BodyText text={text} withDivider />
-          </View>
-        ))}
-        <BodyText text={finalText} />
+        <Title
+          text={RiskMapper[risk]}
+          style={{ textAlign: 'center' }}
+        />
+        <Title text={title} style={{ textAlign: 'center' }} />
+        <BodyText
+          text={contentText[risk]}
+          withDivider />
+        <BodyText text={finalText[risk]} />
       </BodyContainer>
+
       <Dialog
         open={open}
         setOpen={setOpen}
       >
         <View>
-          <Title text="ALTO RISCO" style={{ textAlign: 'center' }} />
+          <Title
+            text={RiskMapper[risk]}
+            style={{ textAlign: 'center' }}
+          />
           <BodyText
-            text={`PACIENTE DE ALTO RISCO${'\n'}Fique atento às intervenções recomendadas`}
+            text={`PACIENTE DE ${RiskMapper[risk] + '\n'}Fique atento às intervenções recomendadas`}
             style={{ textAlign: 'center' }}
           />
         </View>
